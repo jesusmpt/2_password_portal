9830454544fcfa6b7760e5170c20283a8819e628
jesusmpt <jesusmpt@gmail.com>
2025-11-12 20:26:45 +0100

    UX Redesign

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
